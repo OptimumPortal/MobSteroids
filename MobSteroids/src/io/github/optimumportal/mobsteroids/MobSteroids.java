a176be658e30db8e2cacd7c4740acc39a356a84b
OptimumPortal <89494262+OptimumPortal@users.noreply.github.com>
2021-09-02 21:35:06 +1000

    New Config.yml and more customisability!
    There's now a config.yml and you can disable mob modifications.

--- MobSteroids/src/io/github/optimumportal/mobsteroids/MobSteroids.java
+++ MobSteroids/src/io/github/optimumportal/mobsteroids/MobSteroids.java
@@ -1,7 +1,8 @@
 package io.github.optimumportal.mobsteroids;
 
+import java.io.File;
+
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import io.github.optimumportal.mobsteroids.listeners.Listeners;
@@ -9,16 +10,25 @@ import io.github.optimumportal.mobsteroids.listeners.Listeners;
 public class MobSteroids extends JavaPlugin {
   private static MobSteroids instance;
   
-  public static MobSteroids getInstance() {
-    return instance;
-  }
   
+  @Override
   public void onEnable() {
     instance = this;
-    getServer().getPluginManager().registerEvents((Listener)new Listeners(), (Plugin)this);
+    getServer().getPluginManager().registerEvents((Listener)new Listeners(), (org.bukkit.plugin.Plugin)this);
+    
+    File config = new File(getDataFolder() + File.separator + "config.yml");
+    if(!config.exists()) {
+    	this.getConfig().options().copyDefaults(true);
+    	saveDefaultConfig();
+    }  
   }
   
   public void onDisable() {
-    instance = null;
-  }
+    instance = null; //Stops memory leaks.
+  } 
+  
+  public static MobSteroids getInstance() {
+	    return instance; //Access methods from other classes.
 }
+}
+  
--- MobSteroids/src/io/github/optimumportal/mobsteroids/listeners/Listeners.java
+++ MobSteroids/src/io/github/optimumportal/mobsteroids/listeners/Listeners.java
@@ -1,5 +1,6 @@
 package io.github.optimumportal.mobsteroids.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
@@ -10,32 +11,57 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.inventory.ItemStack;
 
+import io.github.optimumportal.mobsteroids.MobSteroids;
+import net.md_5.bungee.api.ChatColor;
+
+
 public class Listeners implements Listener {
-	
+	  boolean ZombieEnabled = MobSteroids.getInstance().getConfig().getBoolean("zombie.enabled");
+	  boolean SkeletonEnabled = MobSteroids.getInstance().getConfig().getBoolean("skeleton.enabled");
+	  boolean ZombieMsgEnabled = MobSteroids.getInstance().getConfig().getBoolean("zombie.enable-modify-message");
+	  boolean SkeletonMsgEnabled = MobSteroids.getInstance().getConfig().getBoolean("skeleton.enable-modify-message");
+	  String prefix = MobSteroids.getInstance().getConfig().getString("prefix");
+	  String ZombieMsg = MobSteroids.getInstance().getConfig().getString("zombie.modify-message");
+	  String SkeletonMsg = MobSteroids.getInstance().getConfig().getString("skeleton.modify-message");
+	  String ConfigZombieHelmet = MobSteroids.getInstance().getConfig().getString("zombie.helmet");
+	  String ConfigZombieChestplate = MobSteroids.getInstance().getConfig().getString("zombie.chestplate");
+	  String ConfigZombieLeggings = MobSteroids.getInstance().getConfig().getString("zombie.leggings");
+	  String ConfigZombieBoots = MobSteroids.getInstance().getConfig().getString("zombie.boots");
+	  
 	@EventHandler
 	public void creatureSpawn(CreatureSpawnEvent event) {
-		
-		
 		if (event.getEntityType() == EntityType.ZOMBIE) {
+			if(ZombieEnabled == true) {
 			Zombie zombie = (Zombie)event.getEntity();
-			
-			zombie.getEquipment().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
-			zombie.getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
-			zombie.getEquipment().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
-			zombie.getEquipment().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
-		}
+			Material zombiehelmet = Material.matchMaterial(ConfigZombieHelmet);
+			Material zombiechestplate = Material.matchMaterial(ConfigZombieChestplate);
+			Material zombieleggings = Material.matchMaterial(ConfigZombieLeggings);
+			Material zombieboots = Material.matchMaterial(ConfigZombieBoots);
+			zombie.getEquipment().setHelmet(new ItemStack(zombiehelmet));
+			zombie.getEquipment().setChestplate(new ItemStack(zombiechestplate));
+			zombie.getEquipment().setLeggings(new ItemStack(zombieleggings));
+			zombie.getEquipment().setBoots(new ItemStack(zombieboots));
+				if (ZombieMsgEnabled == true) {
+					Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + prefix + ZombieMsg);
+				}
+				}
+			}
 		if (event.getEntityType() == EntityType.SKELETON) {
 			Skeleton skeleton = (Skeleton)event.getEntity();
-			
+			if(SkeletonEnabled == true) {
 			skeleton.getEquipment().setHelmet(new ItemStack(Material.DIAMOND_HELMET));
 			skeleton.getEquipment().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
 			skeleton.getEquipment().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
 			skeleton.getEquipment().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
 			
-			ItemStack pogBow = new ItemStack(Material.BOW);
-			pogBow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 2);
-			pogBow.addEnchantment(Enchantment.ARROW_FIRE, 1);
-			skeleton.getEquipment().setItemInMainHand(pogBow);
+			ItemStack skeletonBow = new ItemStack(Material.BOW);
+			skeletonBow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 2);
+			skeletonBow.addEnchantment(Enchantment.ARROW_FIRE, 1);
+			skeleton.getEquipment().setItemInMainHand(skeletonBow);
+			if (SkeletonMsgEnabled == true) {
+				Bukkit.getConsoleSender().sendMessage(ChatColor.AQUA + prefix + SkeletonMsg);
+			}
+			}
 		}
 	}
 }
